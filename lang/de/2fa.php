0096880675b14abce2c112cfd9ea7417b36054e2
cbl <lennart.carbe@gmail.com>
2020-11-15 21:19:11 +0100

    wip

--- lang/de/2fa.php
+++ lang/de/2fa.php
@@ -1,5 +1,27 @@
 <?php
 
 return [
-    '2fa' => 'zwie factor authentication',
+    '2fa'      => 'zwei faktor Authentifizierung',
+    'activate' => '2 Factor Authentifizierung aktivieren',
+    'code'     => 'Authentifizierungscode',
+    'messages' => [
+        'protected'       => 'Dein Konto ist durch Zwei Faktor Authentifizierung geschützt',
+        'info'            => 'Mit Zwei-Faktor-Authentifizierung kannst ausschließlich du auf dein Konto zugreifen – selbst, wenn jemand anders im Besitz deines Passworts ist. <a href="https://www.youtube.com/watch?v=0mvCeNsTa1g" target="_blank">Mehr erfahren.</a>',
+        'how-it-works'    => 'WIE ES FUNKTIONIERT',
+        'when-logging-in' => 'Wenn du dich einloggst, musst du:',
+        'step-1'          => 'Dein passwort eingeben',
+        'step-2'          => 'einen Code eingeben, der von einer App generiert wurde',
+        'activate'        => [
+            'install-app'      => 'Installiere bitte eine Authentication App, z.B:',
+            'scan-qr'          => 'Öffne die Authenticator-App auf deinem Mobilgerät und scanne den QR-Code.',
+            'key'              => 'Du kannst den QR-Code nicht scannen? <br />Konfiguriere deine App mit diesem Schlüssel:',
+            'enter-password'   => 'Gib das Passwort ein, mit dem du dich derzeit anmeldest.',
+            'current-password' => 'Aktuelles Passwort',
+            'step-1'           => 'AUTHENTICATOR-APP INSTALLIEREN',
+            'step-2'           => 'APP KONFIGURIEREN',
+            'step-3'           => 'GIB DEIN AKTUELLES PASSWORT EIN',
+            'step-4'           => 'GIB DEN SECHSSTELLIGEN CODE EIN, DER VON DER APP GENERIERT WURDE',
+        ],
+
+    ],
 ];
